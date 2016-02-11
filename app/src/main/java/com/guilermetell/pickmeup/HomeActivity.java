09ed45a069d2d32902d51c4df515011af46507de
Guilherme Monteiro <guilherme.mdasilva@gmail.com>
2016-02-11 11:35:36 -0400

    removing unnecessary information such as log, Toasts, etc.

--- app/src/main/java/com/guilermetell/pickmeup/HomeActivity.java
+++ app/src/main/java/com/guilermetell/pickmeup/HomeActivity.java
@@ -3,11 +3,8 @@ package com.guilermetell.pickmeup;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -25,10 +22,6 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -55,7 +48,6 @@ public class HomeActivity extends AppCompatActivity {
         MsgTypeSwitch = (Switch) findViewById(R.id.msg_type_switch);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        //http://stackoverflow.com/questions/32615013/is-it-available-to-set-checkselfpermission-on-minimum-sdk-23
         if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
@@ -90,7 +82,6 @@ public class HomeActivity extends AppCompatActivity {
         PickMeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(HomeActivity.this,"PICK ME!", Toast.LENGTH_LONG).show();
                 showCurrentLocation();
             }
         });
@@ -109,21 +100,16 @@ public class HomeActivity extends AppCompatActivity {
             return;
         }
         Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        String cityName = null;
         String SelectedContactNumber = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString(SELECTED_CONTACT_NUMBER, UNKNOWN_CONTACT_NUMBER);
 
-        if (location != null) {
-            cityName = getCityNameFromCoordinates(location);
-        } else {
+        if (location == null) {
             location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             if (location == null) {
                 enableGPS();
                 return;
             }
-            cityName = getCityNameFromCoordinates(location);
         }
         String message = String.format(
-                //"https://www.google.com/maps/@%1$s,%2$s",
                 "http://maps.google.com/maps?daddr=%1$s,%2$s",
                 location.getLatitude(), location.getLongitude()
         );
@@ -136,13 +122,10 @@ public class HomeActivity extends AppCompatActivity {
                 sendSMS(message, SelectedContactNumber);
             }
         }
-        Toast.makeText(HomeActivity.this, message,
-                Toast.LENGTH_LONG).show();
-
     }
 
     public void enableGPS() {
-        LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             Toast.makeText(HomeActivity.this, "Please, turn on you Location Settings",
@@ -151,64 +134,27 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    public String getCityNameFromCoordinates(Location location) {
-        String cityName = null;
-        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
-        List<Address> addresses;
-        try {
-            addresses = gcd.getFromLocation(location.getLatitude(),
-                    location.getLongitude(), 1);
-            if (addresses.size() > 0) {
-                System.out.println(addresses.get(0).getLocality());
-                cityName = addresses.get(0).getLocality();
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return cityName;
-    }
-
     public void onClickWhatsApp(String message) {
-
-        PackageManager pm=getPackageManager();
+        PackageManager pm = getPackageManager();
         try {
-
-            Intent waIntent = new Intent(Intent.ACTION_SEND);
-            waIntent.setType("text/plain");
-
-            PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
-            //Check if package exists or not. If not then code
-            //in catch block will be called
-            waIntent.setPackage("com.whatsapp");
-
-            waIntent.putExtra(Intent.EXTRA_TEXT, message);
-            startActivity(Intent.createChooser(waIntent, "Share with"));
-
+            Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
+            whatsappIntent.setType("text/plain");
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            whatsappIntent.setPackage("com.whatsapp");
+            whatsappIntent.putExtra(Intent.EXTRA_TEXT, message);
+            startActivity(Intent.createChooser(whatsappIntent, "Share with"));
         } catch (PackageManager.NameNotFoundException e) {
-            Toast.makeText(this, "WhatsApp not Installed", Toast.LENGTH_SHORT)
+            Toast.makeText(this, "WhatsApp not Installed", Toast.LENGTH_LONG)
                     .show();
         }
-
     }
 
     private class MyLocationListener implements LocationListener {
 
         public void onLocationChanged(Location location) {
-//            String message = String.format(
-//                    "New Location \n Longitude: %1$s \n Latitude: %2$s",
-//                    location.getLongitude(), location.getLatitude()
-//            );
-//            Toast.makeText(HomeActivity.this, message, Toast.LENGTH_LONG).show();
-//            Toast.makeText(
-//                    getBaseContext(),
-//                    "Location changed: Lat: " + location.getLatitude() + " Lng: "
-//                            + location.getLongitude(), Toast.LENGTH_SHORT).show();
-            String longitude = "Longitude: " + location.getLongitude();
-            Log.d(TAG, longitude);
-            String latitude = "Latitude: " + location.getLatitude();
-            Log.d(TAG, latitude);
-
+            location.getLongitude();
+            location.getLatitude();
+            Log.d(TAG, "Provider Location changed");
         }
 
         public void onStatusChanged(String s, int i, Bundle b) {
@@ -222,7 +168,6 @@ public class HomeActivity extends AppCompatActivity {
         public void onProviderEnabled(String s) {
             Log.d(TAG, "Provider enabled by the user. GPS turned on");
         }
-
     }
 
     public void sendSMS(String message, String phoneNumber) {
@@ -233,7 +178,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private void pickContact() {
         Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
-        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
         startActivityForResult(pickContactIntent, 1);
     }
 
@@ -241,7 +186,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (data != null) {
             Uri uri = data.getData();
-
             if (uri != null) {
                 Cursor c = null;
                 try {
@@ -249,10 +193,8 @@ public class HomeActivity extends AppCompatActivity {
                                     ContactsContract.CommonDataKinds.Phone.NUMBER,
                                     ContactsContract.CommonDataKinds.Phone.TYPE },
                             null, null, null);
-
                     if (c != null && c.moveToFirst()) {
                         String SelectedContactNumber = c.getString(0);
-                        int SelectedContactType = c.getInt(1);
                         PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit().putString(SELECTED_CONTACT_NUMBER, SelectedContactNumber).commit();
                     }
                 } finally {
@@ -266,19 +208,14 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_contact) {
             pickContact();
             return true;
